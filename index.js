74287e35bfbd67d7b490474a9e46d4fed5f06537
OlaPapiGrr <bryan.amrein@gmx.ch>
2024-12-19 15:33:21 +0100

    Changed index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const word1 = generate({ exactly: 1, minLength: 10, wordsPerString: 1 })[0];
 const word2 = generate();
 
 console.log(`The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`);
+console.log(`test`)
 
 export const sum = (a, b) => a + b;
 
