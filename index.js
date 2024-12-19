a47501eb20bdc117a134c43561126023ce53fb05
OlaPapiGrr <bryan.amrein@gmx.ch>
2024-12-19 16:06:20 +0100

    changed index.js 5

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const word2 = generate();
 
 console.log(`The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`);
 console.log(`hallo_welt`)
-let test = 2
+var test = 2
 console.log(test)
 
 export const sum = (a, b) => a + b;
